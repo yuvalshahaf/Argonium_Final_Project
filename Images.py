4abf2900f5c54ab6cfdd49efbaa0d6e27916b092
Yuval Shahaf <yuval.shahaf4102@gmail.com>
2020-02-10 18:54:42 +0200

    Added multipath search

--- Images.py
+++ Images.py
@@ -2,34 +2,34 @@ import os
 
 
 class Images:
-    PATH = ""
+    PATHS = []
 
-    FIGHTER_PATH = ""
+    FIGHTER_PATHS = []
     FIGHTER_MOVE_UP_IMAGES = []
     FIGHTER_MOVE_DOWN_IMAGES = []
     FIGHTER_MOVE_RIGHT_IMAGES = []
     FIGHTER_MOVE_LEFT_IMAGES = []
 
-    MAPS_PATH = ""
-    MAP_TOWN = ""
+    MAPS_PATHS = []
+    MAP_TOWN_PATHS = []
 
     @staticmethod
     def load_images():
         game_folder = "Argonium_Final_Project"
         for path, directories, files in os.walk('C:\\'):
             if game_folder in directories:
-                Images.PATH = os.path.join(path, game_folder) + "\\Pictures\\"
+                Images.PATHS.append(os.path.join(path, game_folder) + "\\Pictures\\")
 
-        Images.FIGHTER_PATH = Images.PATH + "fighter\\"
-        Images.MAPS_PATH = Images.PATH + "maps\\"
+        Images.FIGHTER_PATHS.append(path + "fighter\\" for path in Images.PATHS)
+        Images.MAPS_PATHS.append(path + "maps\\" for path in Images.PATHS)
 
         for i in range(4):
-            Images.FIGHTER_MOVE_UP_IMAGES.append(Images.FIGHTER_PATH + "move_U\\U_" + str(i) + ".png")
-            Images.FIGHTER_MOVE_RIGHT_IMAGES.append(Images.FIGHTER_PATH + "move_R\\R_" + str(i) + ".png")
-            Images.FIGHTER_MOVE_DOWN_IMAGES.append(Images.FIGHTER_PATH + "move_D\\D_" + str(i) + ".png")
-            Images.FIGHTER_MOVE_LEFT_IMAGES.append(Images.FIGHTER_PATH + "move_L\\L_" + str(i) + ".png")
+            Images.FIGHTER_MOVE_UP_IMAGES.append(path + "move_U\\U_" + str(i) + ".png" for path in Images.FIGHTER_PATHS)
+            Images.FIGHTER_MOVE_RIGHT_IMAGES.append(path + "move_R\\R_" + str(i) + ".png" for path in Images.FIGHTER_PATHS)
+            Images.FIGHTER_MOVE_DOWN_IMAGES.append(path + "move_D\\D_" + str(i) + ".png" for path in Images.FIGHTER_PATHS)
+            Images.FIGHTER_MOVE_LEFT_IMAGES.append(path + "move_L\\L_" + str(i) + ".png" for path in Images.FIGHTER_PATHS)
 
-        Images.MAP_TOWN = Images.MAPS_PATH + "town.tmx"
+        Images.MAP_TOWN_PATHS.append(path + "town.tmx" for path in Images.MAPS_PATHS)
 
 
 
--- World.py
+++ World.py
@@ -15,36 +15,3 @@ class World:
 
     def add_new_character(self, player_character):
         self.player_characters.append(player_character)
-
-    def get_new_map(self, direction, current_map_location):
-        directions = {
-            GameActions.MAP_CHANGE_UP: self.change_map_up,
-            GameActions.MAP_CHANGE_DOWN: self.change_map_down,
-            GameActions.MAP_CHANGE_RIGHT: self.change_map_right,
-            GameActions.MAP_CHANGE_LEFT: self.change_map_left
-        }
-        return directions[direction](current_map_location)
-
-    def change_map_up(self, current_map_location):
-        current_map_location[1] += 1
-        for single_map in self.maps:
-            if single_map.map_location == current_map_location:
-                return single_map
-
-    def change_map_down(self, current_map_location):
-        current_map_location[1] -= 1
-        for single_map in self.maps:
-            if single_map.map_location == current_map_location:
-                return single_map
-
-    def change_map_right(self, current_map_location):
-        current_map_location[0] += 1
-        for single_map in self.maps:
-            if single_map.map_location == current_map_location:
-                return single_map
-
-    def change_map_left(self, current_map_location):
-        current_map_location[0] -= 1
-        for single_map in self.maps:
-            if single_map.map_location == current_map_location:
-                return single_map
